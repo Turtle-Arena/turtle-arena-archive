e6b3164a24af7c80d8cdd3bd14d75b20705477b6
Zack Middleton <zturtleman@gmail.com>
2010-01-06 15:22:00 +0000

    ioquake3 resync to revision 1769 from 1759. * (bug #4357) CVAR_ROM+CVAR_ARCHIVE is not read from q3config * That probably deserves a Com_DPrintf as well * Rate limit getstatus and rcon connectionless requests * Fix some compiler warnings update mumble link interface for version 1.2 mmap could fail, handle gracefully set default rate to broadband make reason for voip disable more visible fix mumble coordinate transmission avoid setting cvars by name each frame

--- engine/code/client/cl_scrn.c
+++ engine/code/client/cl_scrn.c
@@ -449,12 +449,6 @@ void SCR_DrawVoipMeter( void ) {
 		return;  // not connected to a server.
 	else if (!cl_connectedToVoipServer)
 		return;  // server doesn't support VoIP.
-	else if (
-#ifndef TMNTSP
-	Cvar_VariableValue( "g_gametype" ) == GT_SINGLE_PLAYER ||
-#endif
-	Cvar_VariableValue("ui_singlePlayerActive"))
-		return;  // single player game.
 	else if (clc.demoplaying)
 		return;  // playing back a demo.
 	else if (!cl_voip->integer)
@@ -666,8 +660,10 @@ void SCR_UpdateScreen( void ) {
 	// that case.
 	if( uivm || com_dedicated->integer )
 	{
+		// XXX
+		extern cvar_t* r_anaglyphMode;
 		// if running in stereo, we need to draw the frame twice
-		if ( cls.glconfig.stereoEnabled || Cvar_VariableIntegerValue("r_anaglyphMode")) {
+		if ( cls.glconfig.stereoEnabled || r_anaglyphMode->integer) {
 			SCR_DrawScreenField( STEREO_LEFT );
 			SCR_DrawScreenField( STEREO_RIGHT );
 		} else {
