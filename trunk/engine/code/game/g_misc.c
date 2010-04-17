049ccf3c114143712f4e2c6d33847d5ccfb6a4cd
ZTurtleMan <ZTurtleMan@f7be2a76-c56c-11de-9fd0-4189a285c7bf>
2010-04-17 16:47:54 +0000

    - Bot shouldn't target players with PW_FLASHING now - Spelling

--- trunk/engine/code/game/g_misc.c
+++ trunk/engine/code/game/g_misc.c
@@ -650,7 +650,7 @@ void misc_object_respawn(gentity_t *self)
 	// Don't let the humans see it respawn
 	if (G_SeenByHumans(self))
 	{
-		G_Printf("DEBUG: respawing deffered...\n");
+		G_Printf("DEBUG: respawing deferred...\n");
 
 		// Try again later
 		self->nextthink = level.time + 1000;
