81c763f3a9e35fd03466eb57adef1d507fac5059
ZTurtleMan <ZTurtleMan@f7be2a76-c56c-11de-9fd0-4189a285c7bf>
2010-08-27 21:52:30 +0000

    - Added support to filter bots out of server list (using "g_humanplayers") in ui/q3_ui

--- trunk/engine/code/ui/ui_local.h
+++ trunk/engine/code/ui/ui_local.h
@@ -68,6 +68,9 @@ extern vmCvar_t	ui_browserGameType;
 extern vmCvar_t	ui_browserSortKey;
 extern vmCvar_t	ui_browserShowFull;
 extern vmCvar_t	ui_browserShowEmpty;
+#ifdef IOQ3ZTM // G_HUMANPLAYERS
+extern vmCvar_t	ui_browserShowBots;
+#endif
 
 extern vmCvar_t	ui_brassTime;
 extern vmCvar_t	ui_drawCrosshair;
