d49fd4194207d66f9c5635b1d7f02d8da5820ce4
ZTurtleMan <ZTurtleMan@f7be2a76-c56c-11de-9fd0-4189a285c7bf>
2011-02-10 05:33:16 +0000

    - Non-hacky fix for auto completing exec and condump filenames of file on disk while on a pure server.

--- trunk/engine/code/client/cl_console.c
+++ trunk/engine/code/client/cl_console.c
@@ -319,7 +319,7 @@ Cmd_CompleteTxtName
 */
 void Cmd_CompleteTxtName( char *args, int argNum ) {
 	if( argNum == 2 ) {
-		Field_CompleteFilename( "", "txt", qfalse );
+		Field_CompleteFilename( "", "txt", qfalse, qtrue );
 	}
 }
 
