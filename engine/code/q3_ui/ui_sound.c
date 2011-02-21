96cd781414a5fb08772da440d3533e9d64ec133f
Zack Middleton <zturtleman@gmail.com>
2011-02-21 01:48:00 +0000

    - Fixed sound quality option in q3_ui to change a setting that exists - Added "Use OpenAL" to sound options in q3_ui - Added apply button to sound options instead of restarting sound/video when changing settings - Increased default base sound quality in engine

--- engine/code/q3_ui/ui_sound.c
+++ engine/code/q3_ui/ui_sound.c
@@ -35,6 +35,10 @@ SOUND OPTIONS MENU
 #define ART_FRAMER			"menu/art/frame1_r"
 #define ART_BACK0			"menu/art/back_0"
 #define ART_BACK1			"menu/art/back_1"
+#ifdef IOQ3ZTM
+#define ART_ACCEPT0			"menu/art/accept_0"
+#define ART_ACCEPT1			"menu/art/accept_1"
+#endif
 
 #define ID_GRAPHICS			10
 #define ID_DISPLAY			11
@@ -45,11 +49,24 @@ SOUND OPTIONS MENU
 #define ID_QUALITY			16
 //#define ID_A3D				17
 #define ID_BACK				18
-
+#ifdef IOQ3ZTM
+#define ID_APPLY			19
+#define ID_OPENAL			20
+#endif
 
 static const char *quality_items[] = {
+#ifdef IOQ3ZTM
+	"Low", "Medium", "High", NULL
+#else
 	"Low", "High", NULL
+#endif
 };
+
+#ifdef IOQ3ZTM
+static const char *openal_items[] = {
+	"No", "Yes", NULL
+};
+#endif
 
 typedef struct {
 	menuframework_s		menu;
@@ -66,9 +83,18 @@ typedef struct {
 	menuslider_s		sfxvolume;
 	menuslider_s		musicvolume;
 	menulist_s			quality;
+#ifdef IOQ3ZTM
+	menulist_s  		openal;
+#endif
 //	menuradiobutton_s	a3d;
 
 	menubitmap_s		back;
+#ifdef IOQ3ZTM
+	menubitmap_s		apply;
+
+	int					quality_default;
+	int					openal_default;
+#endif
 } soundOptionsInfo_t;
 
 static soundOptionsInfo_t	soundOptionsInfo;
@@ -112,6 +138,7 @@ static void UI_SoundOptionsMenu_Event( void* ptr, int event ) {
 		break;
 
 	case ID_QUALITY:
+#ifndef IOQ3ZTM
 		if( soundOptionsInfo.quality.curvalue ) {
 			trap_Cvar_SetValue( "s_khz", 22 );
 			trap_Cvar_SetValue( "s_compression", 0 );
@@ -122,6 +149,7 @@ static void UI_SoundOptionsMenu_Event( void* ptr, int event ) {
 		}
 		UI_ForceMenuOff();
 		trap_Cmd_ExecuteText( EXEC_APPEND, "snd_restart\n" );
+#endif
 		break;
 /*
 	case ID_A3D:
@@ -137,9 +165,72 @@ static void UI_SoundOptionsMenu_Event( void* ptr, int event ) {
 	case ID_BACK:
 		UI_PopMenu();
 		break;
+
+#ifdef IOQ3ZTM
+	case ID_APPLY:
+		switch ( soundOptionsInfo.quality.curvalue ) {
+			case 2:
+				trap_Cvar_SetValue( "s_sdlSpeed", 44100 );
+				break;
+			case 1:
+				trap_Cvar_SetValue( "s_sdlSpeed", 22050 );
+				break;
+			case 0:
+				trap_Cvar_SetValue( "s_sdlSpeed", 11025 );
+				break;
+		}
+
+		trap_Cvar_SetValue( "s_useOpenAL", soundOptionsInfo.openal.curvalue );
+
+		UI_ForceMenuOff();
+		trap_Cmd_ExecuteText( EXEC_APPEND, "snd_restart\n" );
+		break;
+#endif
 	}
 }
 
+#ifdef IOQ3ZTM
+/*
+=================
+GraphicsOptions_UpdateMenuItems
+=================
+*/
+static void SoundOptions_UpdateMenuItems( void )
+{
+	if ( soundOptionsInfo.openal.curvalue == 1 )
+	{
+		soundOptionsInfo.quality.generic.flags |= QMF_GRAYED;
+	}
+	else
+	{
+		soundOptionsInfo.quality.generic.flags &= ~QMF_GRAYED;
+	}
+
+	soundOptionsInfo.apply.generic.flags |= QMF_HIDDEN|QMF_INACTIVE;
+
+	if ( soundOptionsInfo.quality_default != soundOptionsInfo.quality.curvalue )
+	{
+		soundOptionsInfo.apply.generic.flags &= ~(QMF_HIDDEN|QMF_INACTIVE);
+	}
+	if ( soundOptionsInfo.openal_default != soundOptionsInfo.openal.curvalue )
+	{
+		soundOptionsInfo.apply.generic.flags &= ~(QMF_HIDDEN|QMF_INACTIVE);
+	}
+}
+
+/*
+================
+SoundOptions_MenuDraw
+================
+*/
+void SoundOptions_MenuDraw (void)
+{
+//APSFIX - rework this
+	SoundOptions_UpdateMenuItems();
+
+	Menu_Draw( &soundOptionsInfo.menu );
+}
+#endif
 
 /*
 ===============
@@ -148,12 +239,18 @@ UI_SoundOptionsMenu_Init
 */
 static void UI_SoundOptionsMenu_Init( void ) {
 	int				y;
+#ifdef IOQ3ZTM
+	int				speed;
+#endif
 
 	memset( &soundOptionsInfo, 0, sizeof(soundOptionsInfo) );
 
 	UI_SoundOptionsMenu_Cache();
 	soundOptionsInfo.menu.wrapAround = qtrue;
 	soundOptionsInfo.menu.fullscreen = qtrue;
+#ifdef IOQ3ZTM
+	soundOptionsInfo.menu.draw		= SoundOptions_MenuDraw;
+#endif
 
 	soundOptionsInfo.banner.generic.type		= MTYPE_BTEXT;
 	soundOptionsInfo.banner.generic.flags		= QMF_CENTER_JUSTIFY;
@@ -250,6 +347,19 @@ static void UI_SoundOptionsMenu_Init( void ) {
 	soundOptionsInfo.quality.generic.x			= 400;
 	soundOptionsInfo.quality.generic.y			= y;
 	soundOptionsInfo.quality.itemnames			= quality_items;
+
+#ifdef IOQ3ZTM
+	y += BIGCHAR_HEIGHT+2;
+	soundOptionsInfo.openal.generic.type		= MTYPE_SPINCONTROL;
+	soundOptionsInfo.openal.generic.name		= "Use OpenAL:";
+	soundOptionsInfo.openal.generic.flags		= QMF_PULSEIFFOCUS|QMF_SMALLFONT;
+	soundOptionsInfo.openal.generic.callback	= UI_SoundOptionsMenu_Event;
+	soundOptionsInfo.openal.generic.id			= ID_OPENAL;
+	soundOptionsInfo.openal.generic.x			= 400;
+	soundOptionsInfo.openal.generic.y			= y;
+	soundOptionsInfo.openal.itemnames			= openal_items;
+#endif
+
 /*
 	y += BIGCHAR_HEIGHT+2;
 	soundOptionsInfo.a3d.generic.type			= MTYPE_RADIOBUTTON;
@@ -271,6 +381,19 @@ static void UI_SoundOptionsMenu_Init( void ) {
 	soundOptionsInfo.back.height				= 64;
 	soundOptionsInfo.back.focuspic				= ART_BACK1;
 
+#ifdef IOQ3ZTM
+	soundOptionsInfo.apply.generic.type			= MTYPE_BITMAP;
+	soundOptionsInfo.apply.generic.name			= ART_ACCEPT0;
+	soundOptionsInfo.apply.generic.flags		= QMF_RIGHT_JUSTIFY|QMF_PULSEIFFOCUS;//|QMF_HIDDEN|QMF_INACTIVE;
+	soundOptionsInfo.apply.generic.callback		= UI_SoundOptionsMenu_Event;
+	soundOptionsInfo.apply.generic.id			= ID_APPLY;
+	soundOptionsInfo.apply.generic.x			= 640;
+	soundOptionsInfo.apply.generic.y			= 480-64;
+	soundOptionsInfo.apply.width				= 128;
+	soundOptionsInfo.apply.height				= 64;
+	soundOptionsInfo.apply.focuspic				= ART_ACCEPT1;
+#endif
+
 	Menu_AddItem( &soundOptionsInfo.menu, ( void * ) &soundOptionsInfo.banner );
 	Menu_AddItem( &soundOptionsInfo.menu, ( void * ) &soundOptionsInfo.framel );
 	Menu_AddItem( &soundOptionsInfo.menu, ( void * ) &soundOptionsInfo.framer );
@@ -281,12 +404,30 @@ static void UI_SoundOptionsMenu_Init( void ) {
 	Menu_AddItem( &soundOptionsInfo.menu, ( void * ) &soundOptionsInfo.sfxvolume );
 	Menu_AddItem( &soundOptionsInfo.menu, ( void * ) &soundOptionsInfo.musicvolume );
 	Menu_AddItem( &soundOptionsInfo.menu, ( void * ) &soundOptionsInfo.quality );
+#ifdef IOQ3ZTM
+	Menu_AddItem( &soundOptionsInfo.menu, ( void * ) &soundOptionsInfo.openal );
+#endif
 //	Menu_AddItem( &soundOptionsInfo.menu, ( void * ) &soundOptionsInfo.a3d );
 	Menu_AddItem( &soundOptionsInfo.menu, ( void * ) &soundOptionsInfo.back );
+#ifdef IOQ3ZTM
+	Menu_AddItem( &soundOptionsInfo.menu, ( void * ) &soundOptionsInfo.apply );
+#endif
 
 	soundOptionsInfo.sfxvolume.curvalue = trap_Cvar_VariableValue( "s_volume" ) * 10;
 	soundOptionsInfo.musicvolume.curvalue = trap_Cvar_VariableValue( "s_musicvolume" ) * 10;
+#ifdef IOQ3ZTM
+	speed = trap_Cvar_VariableValue( "s_sdlSpeed" );
+	if (speed == 11025)
+		soundOptionsInfo.quality_default = 0;
+	else if (speed == 22050)
+		soundOptionsInfo.quality_default = 1;
+	else // Default, 44100
+		soundOptionsInfo.quality_default = 2;
+	soundOptionsInfo.quality.curvalue = soundOptionsInfo.quality_default;
+	soundOptionsInfo.openal.curvalue = soundOptionsInfo.openal_default = (trap_Cvar_VariableValue( "s_useOpenAL" ) == 1);
+#else
 	soundOptionsInfo.quality.curvalue = !trap_Cvar_VariableValue( "s_compression" );
+#endif
 //	soundOptionsInfo.a3d.curvalue = (int)trap_Cvar_VariableValue( "s_usingA3D" );
 }
 
