2648d3cc5bb745ff0d5fc7a292352be345fb7b07
Zack Middleton <zturtleman@gmail.com>
2011-05-12 05:46:58 +0000

    - Return after cgame registers font, from WolfET.

--- engine/code/client/cl_cgame.c
+++ engine/code/client/cl_cgame.c
@@ -606,6 +606,9 @@ intptr_t CL_CgameSystemCalls( intptr_t *args ) {
 		return re.RegisterShaderNoMip( VMA(1) );
 	case CG_R_REGISTERFONT:
 		re.RegisterFont( VMA(1), args[2], VMA(3));
+#ifdef IOQ3ZTM // IOQ3BUGFIX: Return after cgame registers font, from WolfET.
+		return 0;
+#endif
 	case CG_R_CLEARSCENE:
 		re.ClearScene();
 		return 0;
