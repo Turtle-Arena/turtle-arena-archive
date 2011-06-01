bbb8757a54e517d7691e3367d61c66eedefe4cc8
ZTurtleMan <ZTurtleMan@f7be2a76-c56c-11de-9fd0-4189a285c7bf>
2011-06-01 18:18:42 +0000

    - Added connect menu (only a background shader) to missionpack ui. - missionpack ui fix ups

--- trunk/base/ui/theme_default.h
+++ trunk/base/ui/theme_default.h
@@ -1,7 +1,6 @@
 /*
 ===========================================================================
-Copyright (C) 2007 Open Arena Team
-Copyright (C) 2009-2010 Zack "ZTurtleMan" Middleton
+Copyright (C) 2009-2011 Zack "ZTurtleMan" Middleton
 
 This file is part of Turtle Arena source code.
 
@@ -21,18 +20,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
-#define THEME_BACKGROUND "ui/assets/splash22.jpg" // background of the menu
-#define THEME_SND_SELECT "ui/assets/select.wav" // confirm noise
-#define THEME_SND_HOVER "ui/assets/hover.wav" // hover noise
+#define THEME_BACKGROUND "ui/assets/out_menuback" // menu background
 
-#define THEME_TITLECOLOR 1 1 1 1 // Title colors
-#define THEME_TEXTCOLOR  0.24 0.70 0.935 1 // regular color of menu button text
-#define THEME_FOCUSCOLOR 0.34 0.8 0.835 1 // focus 
-#define THEME_BOX_HEADERCOLOR 1 1 1 1 // for the text appearing above editboxes
-#define THEME_SMALLCOLOR 0.25 0.45 0.85 1 // the smaller options colors
-
-#define THEME_BOX_BORDER 	0.25 0.25 0.25 1
-#define THEME_BOX_FORE 	0.01   0.5   1   1
-#define THEME_BOX_BACK 	0.0 0.0 0.1 1
-#define THEME_BOX_OUTLINE 	0.01   0.5   1   0.3
+#define THEME_SND_MENU_ENTER "sound/misc/menu1.wav"
+#define THEME_SND_FOCUS "sound/misc/menu2.wav"
+#define THEME_SND_MENU_EXIT "sound/misc/menu3.wav"
+#define THEME_SND_BUZZ "sound/misc/menu4.wav"
 
