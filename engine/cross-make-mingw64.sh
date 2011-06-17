580df9428a9b70e0808333672842345f4fd54431
Zack Middleton <zturtleman@gmail.com>
2011-06-17 18:14:49 +0000

    - ioquake3 resync to revision 2041 from 2024.

--- engine/cross-make-mingw64.sh
+++ engine/cross-make-mingw64.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-export CC=amd64-mingw32msvc-gcc
-export WINDRES=amd64-mingw32msvc-windres
+if [ !$CC ]
+then
+  export CC=amd64-mingw32msvc-gcc
+fi
+
+if [ !$WINDRES ]
+then
+  export WINDRES=amd64-mingw32msvc-windres
+fi
+
 export PLATFORM=mingw32
 export ARCH=x64
+
 exec make $*
