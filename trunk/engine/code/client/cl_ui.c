640fd6cde2c6c4398d94ced7633a3a3956adc519
ZTurtleMan <ZTurtleMan@f7be2a76-c56c-11de-9fd0-4189a285c7bf>
2011-11-03 21:34:32 +0000

    ioq3ztm resync to revision 62 from 57.

--- trunk/engine/code/client/cl_ui.c
+++ trunk/engine/code/client/cl_ui.c
@@ -1123,8 +1123,12 @@ void CL_InitUI( void ) {
 	}
 
 	// sanity check
-	v = VM_Call( uivm, UI_GETAPIVERSION );
+	v = VM_SafeCall( uivm, UI_GETAPIVERSION );
 	if (v != UI_API_VERSION) {
+		// Free uivm now, so UI_SHUTDOWN doesn't get called later.
+		VM_Free( uivm );
+		uivm = NULL;
+
 		Com_Error( ERR_DROP, "User Interface is version %d, expected %d", v, UI_API_VERSION );
 		cls.uiStarted = qfalse;
 	}
