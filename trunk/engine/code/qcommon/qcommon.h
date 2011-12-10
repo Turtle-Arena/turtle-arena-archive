d070f8e2c2c5b4a10d7f54b3f269ec4b7a2e35e0
ZTurtleMan <ZTurtleMan@f7be2a76-c56c-11de-9fd0-4189a285c7bf>
2011-12-10 00:02:02 +0000

    Resync ioq3ztm to revision 87 from 86.

--- trunk/engine/code/qcommon/qcommon.h
+++ trunk/engine/code/qcommon/qcommon.h
@@ -935,7 +935,7 @@ temp file loading
 
 */
 
-#if defined(_DEBUG) && !defined(BSPC)
+#if !defined(NDEBUG) && !defined(BSPC)
 	#define ZONE_DEBUG
 #endif
 
@@ -965,7 +965,6 @@ void *Hunk_AllocateTempMemory( int size );
 void Hunk_FreeTempMemory( void *buf );
 int	Hunk_MemoryRemaining( void );
 void Hunk_Log( void);
-void Hunk_Trash( void );
 
 void Com_TouchMemory( void );
 
