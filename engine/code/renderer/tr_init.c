79548768823cd8f9c2f048661b7db5a95905f295
Zack Middleton <zturtleman@gmail.com>
2011-12-21 18:07:43 +0000

    Resync to ioq3ztm revision 90 from 87.

--- engine/code/renderer/tr_init.c
+++ engine/code/renderer/tr_init.c
@@ -32,6 +32,10 @@ glstate_t	glState;
 
 static void GfxInfo_f( void );
 
+#ifdef USE_RENDERER_DLOPEN
+cvar_t  *com_altivec;
+#endif
+
 cvar_t	*r_flareSize;
 cvar_t	*r_flareFade;
 cvar_t	*r_flareCoeff;
@@ -1181,6 +1185,10 @@ R_Register
 */
 void R_Register( void ) 
 {
+	#ifdef USE_RENDERER_DLOPEN
+	com_altivec = ri.Cvar_Get("com_altivec", "1", CVAR_ARCHIVE);
+	#endif	
+
 	//
 	// latched and archived variables
 	//
