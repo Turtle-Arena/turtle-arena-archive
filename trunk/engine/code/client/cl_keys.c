28b3bfc480962ae80b98ea54bc48fa57f75473af
ZTurtleMan <ZTurtleMan@f7be2a76-c56c-11de-9fd0-4189a285c7bf>
2012-02-06 23:37:22 +0000

    Merged some TA_MAIN // BRANDING and resync some code to ioq3ztm.

--- trunk/engine/code/client/cl_keys.c
+++ trunk/engine/code/client/cl_keys.c
@@ -1595,11 +1595,7 @@ void Key_SetCatcher( int catcher ) {
 
 // This must not exceed MAX_CMD_LINE
 #define			MAX_CONSOLE_SAVE_BUFFER	1024
-#ifdef TA_MAIN // BRANDING
 #define			CONSOLE_HISTORY_FILE    "history"
-#else
-#define			CONSOLE_HISTORY_FILE    "q3history"
-#endif
 static char	consoleSaveBuffer[ MAX_CONSOLE_SAVE_BUFFER ];
 static int	consoleSaveBufferSize = 0;
 
