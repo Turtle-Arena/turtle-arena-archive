e95bf4da0a24d3afbc391fa41831ada2e60f0975
Zack Middleton <zturtleman@gmail.com>
2012-12-22 05:23:53 +0000

    Spearmint resync to revision 366 from 364.
    Move console notify area from client to cgame, based on code from Tremulous.
    Add trap_LiteralArgs to cgame, game, and ui, based on code from Tremulous.
    Give each local client their own console notify area.
    Telling now uses separate "tell" instead of "chat", so it's only show to correct local clients.
    "print"s sent to everyone are now changed to "gprint" so cgame knows it's for all local clients. So "print"s sent directly to a local client are only printed in their notify area.

--- engine/code/cgame/cg_draw.c
+++ engine/code/cgame/cg_draw.c
@@ -3756,6 +3756,47 @@ static void CG_DrawWarmup( void ) {
 #endif
 }
 
+
+/*
+=====================
+CG_DrawSmallWrappedText
+
+Draw multiline text
+=====================
+*/
+void CG_DrawSmallWrappedText(int x, int y, const char *textPtr) {
+	const char *p, *start;
+	char buff[1024];
+
+	if (!textPtr || *textPtr == '\0') {
+		return;
+	}
+
+	start = textPtr;
+	p = strchr(textPtr, '\n');
+	while (p && *p) {
+		strncpy(buff, start, p-start+1);
+		buff[p-start] = '\0';
+		CG_DrawSmallString(x, y, buff, 1.0f );
+		y += SMALLCHAR_HEIGHT + 3;
+		start += p - start + 1;
+		p = strchr(p+1, '\n');
+	}
+	CG_DrawSmallString(x, y, start, 1.0f );
+}
+
+/*
+=====================
+CG_DrawNotify
+
+Draw console notify area.
+=====================
+*/
+void CG_DrawNotify( void ) {
+	CG_SetScreenPlacement(PLACE_LEFT, PLACE_TOP);
+	CG_DrawSmallWrappedText(0, 0, cg.cur_lc->consoleText);
+}
+
 //==================================================================================
 #ifdef MISSIONPACK_HUD
 /* 
@@ -3962,6 +4003,8 @@ static void CG_Draw2D(stereoFrame_t stereoFrame)
 #endif
 		CG_DrawCenterString();
 	}
+
+	CG_DrawNotify();
 }
 
 
